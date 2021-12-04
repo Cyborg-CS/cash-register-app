a5ca4f6720de235e06fbe2897121ef24ae1f67c6
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2021-12-04 15:27:19 +0530

    test drive

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check-btn");
+const totalChange = document.querySelector("#total-change");
+
+console.log(billAmount.value);
+// checkBtn.addEventListener("click", console.log(`Clicked !${cashGiven.value}`))
